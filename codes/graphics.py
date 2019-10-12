fcff03326660769cf612a09332387601240ea611
rhc <chenhairong@itouchtv.cn>
2019-10-12 17:10:31 +0800

    graphics.py

---
+++ codes/graphics.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+# https://blog.csdn.net/weixin_44037416/article/details/96842058
+
+import os
+from PIL import Image
+
+def PILTest():
+    catIm = Image.open('./PythonAutomation/project/automate_online-materials/zophie.png')
+
+
+
+PILTest()
